be68cb552fe17114c2d52178feca3548f89511ef
pradeep kumar <praj7330@gmail.com>
2017-02-26 12:07:02 -0800

    Add files via upload

---
+++ Q2/main.sh
@@ -0,0 +1,20 @@
+javac boy.java
+javac miser_boy.java
+javac generous_boy.java
+javac geeks_boy.java
+javac girl.java
+javac choosy_girl.java
+javac normal_girl.java
+javac desperate_girl.java
+javac gift.java
+javac luxury_gift.java
+javac utility_gift.java
+javac couple.java
+javac write_boys2.java
+javac write_girls2.java
+javac write_file2.java
+javac ques_1_algo.java
+javac ques_2_algo.java
+javac main2.java
+java main2
+cat boys2.csv girls2.csv event2.txt
